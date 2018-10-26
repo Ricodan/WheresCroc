029eb249a96c9eb77bf3a7113e3f058f7e423119
ricodan <danzaricardo7@gmail.com>
2018-10-26 17:36:34 +0200

    added some basic functions

--- WheresCroc.R
+++ WheresCroc.R
@@ -7,7 +7,9 @@ transitionMatrix <-function (myPosition, edges){
   
   theMatrix = matrix(nrow=40, ncol = 40)
   for(i in 1:40){
-    neighbors = getNeighbors(myPosition,edges)
+    neighbors = getNeighbors(i,edges)
+    
+    #index[i,i] will remain with probability 0. Keep in mind for later.
     theMatrix[i,] = 0
     #no need fpr loops 
     #divide by the amount of neighbors plus itself since there's a possibility of no movement
@@ -22,18 +24,40 @@ getNeighbors <- function(position, edges) {
      c(edges[which(edges[,1]==position),2],edges[which(edges[,2]==position),1])
 }
   
-  
-  
 
 
+#READINGS: A vector giving the salinity, phosphate and nitrogen reading from Croc sensors at his current location. 
+#TOURISTS: A vector giving the positions of the two tourists (elements 1 and 2) and yourself (element 3). 
+#If a tourist has just been eaten by Croc that turn, the position will be multiplied by -1. If a tourist was eaten by Croc in a 
+#previous turn, then the position will be NA.
+#EDGES: is edges
+#MATRICES: contains the mean[1] and sd[2] of $salinity $phosphate and $nitrate
 myFunction <- function(movesAndMem, readings, tourists, edges, matrices){
-  print(matrices)
+  tmatrix = transitionMatrix(1, edges)
+ 
+  
+ 
+  #Step 3: Create Omissions matrix
+  
+  Fprev = matrix(1/40,ncol = 40)
+  
+  #dnorm for each of the matrices, salinity and stuff like that
+ 
+  #gotta fix this a little bit
+  salinityColumn =  apply(matrices$salinity, 1, function(row) dnorm(readings[1],row[1], row[2]) )
+  phosphateColumn = apply(matrices$phosphate,1, function(row) dnorm(readings[2],row[1], row[2]) )
+  nitrateColumn =   apply(matrices$nitrogen,  1, function(row) dnorm(readings[3],row[1], row[2]) )
+  print(salinityColumn)
+  print(phosphateColumn)
+
   
   
-  #Step 2: Create Fprev
-  #Step 3: Create Omissions
-  
-  Fnew = matrix(0,ncol = 40)
   
   
+  
+  
+  
+  
+  Fnew = Fprev %*% tmatrix
+ 
 }
